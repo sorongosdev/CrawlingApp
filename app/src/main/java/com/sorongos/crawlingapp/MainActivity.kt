7a46569a4df733484c5bac3e8bc192504902ca62
Sorongos <dosora9@gmail.com>
2023-10-29 17:36:52 +0900

    네이버 마크 불러오기

--- app/src/main/java/com/sorongos/crawlingapp/MainActivity.kt
+++ app/src/main/java/com/sorongos/crawlingapp/MainActivity.kt
@@ -1,75 +1,60 @@
 package com.sorongos.crawlingapp
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
-import android.text.TextUtils.isEmpty
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.sorongos.crawlingapp.databinding.ActivityMainBinding
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
-import java.io.IOException
-
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-//    private lateinit var bundle: Bundle
-    private val URL: String =
-        "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%95%88%EC%82%B0+%EB%82%A0%EC%94%A8"
 
-    var handler: Handler = object : Handler(Looper.getMainLooper()) {
-        override fun handleMessage(msg: Message) {
-            Log.d("handleMessage","handleMessage")
-            val bundle = msg.data    //new Thread에서 작업한 결과물 받기
-            Log.d("bundle","${bundle.getString("temperature")}")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            binding.textView.text = bundle.getString("temperature")    //받아온 데이터 textView에 출력
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val url = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query=%EC%95%88%EC%82%B0+%EB%82%A0%EC%94%A8"
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val imageUrl = getImageUrl(url) // 대상 웹 페이지의 URL을 입력
+            Log.d("imgUrl","$imageUrl")
+            imageUrl?.let {
+                launch(Dispatchers.Main) {
+                    loadImageIntoImageView(it, imageView)
+                }
+            }
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        Thread {
-            run {
-                try {
-                    val doc: Document = Jsoup.connect(URL).get() //URL 웹사이트에 있는 html 코드를 다 끌어오기
-
-                    val temele: Elements =
-                        doc.select(".temperature_text") //끌어온 html  에서 클래스네임이 "temperature_text" 인 값만 선택해서 빼오기
-
-                    var isEmpty = temele.isEmpty() //빼온 값 null체크
-
-                    Log.d("Tag", "isNull? : $isEmpty") //로그캣 출력
-
-                    if (isEmpty === false) { //null값이 아니면 크롤링 실행
-                        var tem = temele[0].text()
-                            .substring(5) //크롤링 하면 "현재온도30'c" 이런식으로 뽑아와지기 때문에, 현재온도를 잘라내고 30'c만 뽑아내는 코드
-                        Log.d("로그", "temperature : $tem") //로그캣 출력
-                        val bundle = Bundle()
-                        bundle.putString(
-                            "temperature",
-                            tem
-                        ) //bundle 이라는 자료형에 뽑아낸 결과값 담아서 main Thread로 보내기
-
-                        // 핸들러를 통해 메인 스레드로 데이터 전달
-                        val message = handler.obtainMessage(0)
-                        message.data = bundle
-                        Log.d("로그", "message : $message") //로그캣 출력
-                        handler.sendMessage(message)
-
-                    }
-                    //크롤링 할 구문
-                } catch (e: IOException) {
-                    e.printStackTrace()
+    private fun getImageUrl(url: String): String? {
+        return try {
+            val document: Document = Jsoup.connect(url).get()
+            val imgElements: Elements = document.select("img")
+            if (imgElements.isNotEmpty()) {
+                val imageUrl = imgElements[0].absUrl("src")
+                if (imageUrl.isNotEmpty()) {
+                    imageUrl
+                } else {
+                    null
                 }
+            } else {
+                null
             }
-        }.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
 
+    private fun loadImageIntoImageView(imageUrl: String, imageView: ImageView) {
+        Glide.with(this)
+            .load(imageUrl)
+            .into(imageView)
+    }
 }
